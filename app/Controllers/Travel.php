bff5065341a5f4850a8e553d1a86f736236051fb
AHuangjinhui <2524500884@qq.com>
2020-11-22 22:03:40 +0800

    Added an index page

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -9,12 +9,48 @@
  // retrieve all the records
  $records = $places->findAll();
  
+ 
+ $table = new \CodeIgniter\View\Table();
+ 
+ $headings = $places->fields;
+$displayHeadings = array_slice($headings, 1, 2);
+$table->setHeading(array_map('ucfirst', $displayHeadings));
+ 
+ foreach ($records as $record) {
+     $nameLink1 = '<img src="/image/'.$record->image.'" width="50%" height="50%">';
+$nameLink = anchor("travel/showme/$record->id",$record->name);
+$table->addRow($nameLink,$nameLink1,$record->description);
+}
+
+
+ 
+   $template = [
+'table_open' => '<table cellpadding="5px">',
+'cell_start' => '<td style="border: 1px solid #dddddd;">',
+'row_alt_start' => '<tr style="background-color:#dddddd">',
+];
+   
+   
+   
+   $fields = [
+ 'title' => 'My list of comedians',
+ 'heading' => 'My list of comedians',
+ 'footer' => 'Copyright HuangJinhui'
+ ];
+$table->setTemplate($template);
  // get a template parser
  $parser = \Config\Services::parser();
  // tell it about the substitions
- return $parser->setData(['records' => $records])
+// return $parser->setData(['records' => $records])
  // and have it render the template with those
- ->render('placeslist');
+// ->render('placeslist');
+ 
+return $parser->setData($fields)
+ ->render('templates\top') .
+ $table->generate() .
+ $parser->setData($fields)
+ ->render('templates\bottom');
+ 
  }
  
  public function showme($id)
@@ -24,11 +60,48 @@
  // retrieve all the records
  $record = $places->find($id);
  
+ 
+  $table = new \CodeIgniter\View\Table();
+ 
+ $headings = $places->fields;
+$displayHeadings = array_slice($headings, 0, 7);
+$table->setHeading(array_map('ucfirst', $displayHeadings));
+ 
+// foreach ($records as $record) {
+//$nameLink = anchor("travel/showme/$record->id",$record->name);
+    $nameLink = '<img src="/image/'.$record['image'].'">';
+$table->addRow($record['id'],$record['name'],$record['link'],$record['city'],$record['Magnum opus'],$record['description'],$record['constellation'],$nameLink);
+//}
+
+
+ 
+   $template = [
+'table_open' => '<table cellpadding="5px">',
+'cell_start' => '<td style="border: 1px solid #dddddd;">',
+'row_alt_start' => '<tr style="background-color:#dddddd">',
+];
+   
+   
+   
+   $fields = [
+ 'title' => 'My list of comedians',
+ 'heading' => 'My list of comedians',
+ 'footer' => 'Copyright HuangJinhui'
+ ];
+$table->setTemplate($template);
+ 
+ 
  // get a template parser
  $parser = \Config\Services::parser();
  // tell it about the substitions
- return $parser->setData($record)
+// return $parser->setData($record)
  // and have it render the template with those
- ->render('oneplace');
+// ->render('oneplace');
+ 
+ return $parser->setData($fields)
+ ->render('templates\top') .
+ $table->generate() .
+ $parser->setData($fields)
+ ->render('templates\bottom');
  }
  }
--- app/Models/Places.php
+++ app/Models/Places.php
@@ -1,93 +1,97 @@
 <?php
 namespace App\Models;
+use App\Models\Simple\JSONModel;
 
 /*
  * Mock travel destination data.
  * Note that we don't have to extend CodeIgniter's model for now
  */
 
-class Places {
+class Places extends JSONModel{
 
-    //mock data : an array of records
-    protected $data = [
-        '1' => [
-            'id' => 1,
-            'name' => 'Stephen Chow',
-            'description' => 'Stephen Chow was born on June 22, 1962, in Hong Kong, China, and is originally from Ningbo, Zhejiang Province.
-Chinese film and television actor, director, screenwriter, producer, businessman, CPPCC member, graduated from TVB artist training class.
-National first class actor.',
-            'link' => 'https://baike.baidu.com/item/%E5%91%A8%E6%98%9F%E9%A9%B0/169917?fr=aladdin',
-            'city' => ' Hong Kong, China',
-            'Magnum opus' => '《Kung fu》   《Shaolin soccer》  《The little mermaid》  《A Chinese Odyssey....》',
-            'constellation' => 'Cancer',
-            'image' => 'zxc.jpeg',
-           
-        ],
-        '2' => [
-            'id' => 2,
-            'name' => 'yue yunpeng',
-            'description' => 'Yue Yunpeng, whose real name is Yue Longgang, was born in Puyang, Henan province on April 15, 1985. He is a Chinese actor in crosstalk, film and television.
-                In 2004, he devoted himself to cross talk and became a teacher of Guo Degang, specializing in cross talk, taiping lyrics and bamboo blackboard writing.
-                 He made his debut in 2005.',
-            'link' => 'https://haokan.baidu.com/v?vid=1486458867003045623&pd=bjh&fr=bjhauthor&type=video',
-            'city' => 'Puyang city, Henan Province',
-            'Magnum opus' => '《Pancakes man》   《From all over your world》   《The song of the rings》 ',
-            'constellation' => 'Aries',
-            'image' => 'xiaoyueyue.jpg',
-        ],
-        '3' => [
-            'id' => 3,
-            'name' => 'guo degang',
-            'description' => 'Guo Degang, male, born in Tianjin city on January 18, 1973, crosstalk actor, film, TV drama actor, TV talk show host.
-                In 1979, he devoted himself to the art world. He first learned storytelling from Gao Qinghai, a senior storyteller, and then learned crosstalk from Crosstalk master Chang Baofeng and crosstalk master Hou Yaowen.
-                Meanwhile, I also learned Peking Opera, Pingju opera, Hebei Clapper Opera and other operas.',
-            'link' => 'https://3g.163.com/v/video/VJ18CK3HM.html',
-            'city' => 'tianjin',
-            'Magnum opus' => '《All my life》   《Lost On The Road》   《I am too happy to》 ',
-            'constellation' => 'Capricornus',
-            'image' => 'guodegang.jpg',
-        ],
-        '4' => [
-            'id' => 4,
-            'name' => 'shenteng',
-            'description' => 'Shen Teng, born in Qiqihar, Heilongjiang Province, China on October 23, 1979, is an actor, film director and screenwriter in mainland China. He is a contract artist of Mahua Happy Stage play. He graduated from the Theater performance Department of the PLA Art Academy..',
-            'link' => 'https://www.iqiyi.com/a_19rrhrmmtp.html',
-            'city' => 'Qiqihar city, Heilongjiang Province',
-            'Magnum opus' => '《Charlotte worry》   《The richest man in Xihong city》   《Fast life》 ',
-            'constellation' => 'Libra',
-            'image' => 'shenteng.jpeg',
-        ],
-        '5' => [
-            'id' => 5,
-            'name' => 'Wang baoqiang',
-            'description' => 'Wang Baoqiang was born in Xingtai city, Hebei Province on April 29, 1982. He is an actor and director in the Chinese mainland.',
-            'link' => 'https://baike.baidu.com/item/%E4%BA%BA%E5%9C%A8%E5%9B%A7%E9%80%94/7585422?fr=aladdin',
-            'city' => 'Xingtai City, Hebei Province',
-            'Magnum opus' => '《Detective Chinatown》   《Lost On Journey》   《blind shaft》 ',
-            'constellation' => 'Libra',
-            'image' => 'wangbaoqiang.jpeg',
-        ],
-        '6' => [
-            'id' => 6,
-            'name' => 'Wu mengda',
-            'description' => 'Wu Mengda, born in Xiamen, Fujian Province on January 2, 1953, moved to Hong Kong with his parents at the age of seven. He is a Hong Kong, China actor.',
-            'link' => 'https://baike.baidu.com/item/%E6%B5%81%E6%B5%AA%E5%9C%B0%E7%90%83/16278407?fr=aladdin',
-            'city' => 'Fujian xiamen',
-            'Magnum opus' => '《Fight Back to School》   《Hail the Judge 》   《The Wandering Earth》 ',
-            'constellation' => 'Capricornus',
-            'image' => 'dashu.jpg',
-        ],
-    ];
-
-    public function findAll() {
-        return $this->data;
-    }
-
-    public function find($id = null) {
-        if (!empty($id) && isset($this->data[$id])) {
-            return $this->data[$id];
-        }
-        return null;
-    }
-
+protected $origin = WRITEPATH . 'data/placesData.json';    
+protected $keyField = 'id';
+protected $validationRules = [];
 }
+   //mock data : an array of records
+//    protected $data = [
+//        '1' => [
+//            'id' => 1,
+//            'name' => 'Stephen Chow',
+//            'description' => 'Stephen Chow was born on June 22, 1962, in Hong Kong, China, and is originally from Ningbo, Zhejiang Province.
+//Chinese film and television actor, director, screenwriter, producer, businessman, CPPCC member, graduated from TVB artist training class.
+//National first class actor.',
+//            'link' => 'https://baike.baidu.com/item/%E5%91%A8%E6%98%9F%E9%A9%B0/169917?fr=aladdin',
+//            'city' => ' Hong Kong, China',
+//            'Magnum opus' => '《Kung fu》   《Shaolin soccer》  《The little mermaid》  《A Chinese Odyssey....》',
+//            'constellation' => 'Cancer',
+//            'image' => 'zxc.jpeg',
+//           
+//        ],
+//        '2' => [
+//            'id' => 2,
+//            'name' => 'yue yunpeng',
+//            'description' => 'Yue Yunpeng, whose real name is Yue Longgang, was born in Puyang, Henan province on April 15, 1985. He is a Chinese actor in crosstalk, film and television.
+//                In 2004, he devoted himself to cross talk and became a teacher of Guo Degang, specializing in cross talk, taiping lyrics and bamboo blackboard writing.
+//                 He made his debut in 2005.',
+//            'link' => 'https://haokan.baidu.com/v?vid=1486458867003045623&pd=bjh&fr=bjhauthor&type=video',
+//            'city' => 'Puyang city, Henan Province',
+//            'Magnum opus' => '《Pancakes man》   《From all over your world》   《The song of the rings》 ',
+//            'constellation' => 'Aries',
+//            'image' => 'xiaoyueyue.jpg',
+//        ],
+//        '3' => [
+//            'id' => 3,
+//            'name' => 'guo degang',
+//            'description' => 'Guo Degang, male, born in Tianjin city on January 18, 1973, crosstalk actor, film, TV drama actor, TV talk show host.
+//                In 1979, he devoted himself to the art world. He first learned storytelling from Gao Qinghai, a senior storyteller, and then learned crosstalk from Crosstalk master Chang Baofeng and crosstalk master Hou Yaowen.
+//                Meanwhile, I also learned Peking Opera, Pingju opera, Hebei Clapper Opera and other operas.',
+//            'link' => 'https://3g.163.com/v/video/VJ18CK3HM.html',
+//            'city' => 'tianjin',
+//            'Magnum opus' => '《All my life》   《Lost On The Road》   《I am too happy to》 ',
+//            'constellation' => 'Capricornus',
+//            'image' => 'guodegang.jpg',
+//        ],
+//        '4' => [
+//            'id' => 4,
+//            'name' => 'shenteng',
+//            'description' => 'Shen Teng, born in Qiqihar, Heilongjiang Province, China on October 23, 1979, is an actor, film director and screenwriter in mainland China. He is a contract artist of Mahua Happy Stage play. He graduated from the Theater performance Department of the PLA Art Academy..',
+//            'link' => 'https://www.iqiyi.com/a_19rrhrmmtp.html',
+//            'city' => 'tianjin',
+//            'Magnum opus' => '《Charlotte worry》   《The richest man in Xihong city》   《Fast life》 ',
+//            'constellation' => 'Libra',
+//            'image' => 'shenteng.jpeg',
+//        ],
+//        '5' => [
+//            'id' => 5,
+//            'name' => 'Wang baoqiang',
+//            'description' => 'Wang Baoqiang was born in Xingtai city, Hebei Province on April 29, 1982. He is an actor and director in the Chinese mainland.',
+//            'link' => 'https://baike.baidu.com/item/%E4%BA%BA%E5%9C%A8%E5%9B%A7%E9%80%94/7585422?fr=aladdin',
+//            'city' => 'Xingtai City, Hebei Province',
+//            'Magnum opus' => '《Detective Chinatown》   《Lost On Journey》   《blind shaft》 ',
+//            'constellation' => 'Libra',
+//            'image' => 'wangbaoqiang.jpeg',
+//        ],
+//        '6' => [
+//            'id' => 6,
+//            'name' => 'Wu mengda',
+//            'description' => 'Wu Mengda, born in Xiamen, Fujian Province on January 2, 1953, moved to Hong Kong with his parents at the age of seven. He is a Hong Kong, China actor.',
+//            'link' => 'https://baike.baidu.com/item/%E6%B5%81%E6%B5%AA%E5%9C%B0%E7%90%83/16278407?fr=aladdin',
+//            'city' => 'Fujian xiamen',
+//            'Magnum opus' => '《Fight Back to School》   《Hail the Judge 》   《The Wandering Earth》 ',
+//            'constellation' => 'Capricornus',
+//            'image' => 'dashu.jpg',
+//        ],
+//    ];
+//
+//    public function findAll() {
+//        return $this->data;
+//    }
+//    public function find($id = null) {
+//        if (!empty($id) && isset($this->data[$id])) {
+//            return $this->data[$id];
+//        }
+//        return null;
+//    }
+//
+//}
